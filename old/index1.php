b355a119f80d3277c0c1ec6683815d429098a5c8
EvanJones18 <48502664+EvanJones18@users.noreply.github.com>
2019-04-23 22:31:43 -0700

    add

---
+++ old/index1.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>J. Sydney Jones</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    
+    </head>
+    <body>
+        <div class="container-fluid bg-dark" >
+            <?php include("includes/navigation.html"); ?>
+
+
+
+            
+            <div class="container ">
+
+                <p class="mt-sm-3 mb-sm-3"></p>
+                
+                  <div class="container">
+                  
+                    <div class="card bg-dark text-white">
+                    <img src="image/Banner.jpg" class="card-img" alt="...">
+                    <div class="card-img-overlay">
+                    <h2>J. Sydney Jones, Author</h2>
+                    <p class="card-text">Additional information</p>
+
+                </div>
+              </div>
+            <!--content of index-->
+            <p class="mt-sm-3 mb-sm-3"></p>
+              <div class="card bg-white text-black">
+                <div class="card-body">  
+              <h3>Newest Publications</h3>
+            
+              <?php include("book/edit.html");?>
+</div>
+</div>
+            
+
+
+            
+            
+            
+            
+            
+            
+              </div>
+                
+
+              
+                <p class="mt-sm-5 mb-sm-5"></p>
+            </div>    
+        </div>
+
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    <div class="container-fluid">
+        <?php include("includes/bottomnavigation.html"); ?>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html> 
+
+
