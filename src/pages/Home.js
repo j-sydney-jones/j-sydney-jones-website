e1c09f237d91e9ea4600a7bb2cdbea1b5097be6c
Evan Jones <evan@Evans-iMac.hsd1.ca.comcast.net>
2019-05-30 20:47:41 -0700

    a

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,10 +12,40 @@ export default class Home extends Component {
         <p className="attr-lg">&mdash; San Francisco Book Review</p>
         <p className="p-sm" />
 
-        <div className="card">
-          <div className="card-body">
-            <h2>About the author</h2>
+        <div classname="row">
+          <div className="col-md-7">
+            <div className="card shd-lg">
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-md-2">
+
+                  </div>
+                  <div className="col-md-10">
+                    
+                  </div>
+                </div>
+                
+                <h2>About the author</h2>
+                <p>
+                  The author of travel guides, biographies, and thrillers and
+                  mysteries for adults, J. Sydney Jones pursues a central theme
+                  in all of his work: illumination of the past in order to cast
+                  light on the present. In his thrillers as well as his works of
+                  nonfiction, the emphasis is on historical insight.
+                </p>
+                <p>
+                  Born on afarm in northeastern South Dakota, Jones is one of
+                  four siblings. With the death of his father when Jones was
+                  five, the family moved to Oregon, where Jones grew up a small
+                  coastal town. He attended university in Oregon, and then
+                  studied abroad, an experience that changed his life and made
+                  him want to become a writer.
+                </p>
+                <a className="btn" href="/about">Read more &rarr;</a>
+              </div>
+            </div>
           </div>
+          <div className="col-md-5" />
         </div>
       </div>
     );
