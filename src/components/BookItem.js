37ddd62ef416fb7917853d4e216df64cbb264567
Evan Jones <evan@Evans-iMac.local>
2019-06-12 19:29:25 -0700

    a

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -7,7 +7,7 @@ export default class BookItem extends Component {
         return (
             <div style={{margin: "0rem 2rem 2rem 2rem", zIndex: 1}} >
                 <a href={this.props.href} >
-                    <img className="book-item" src={require(`../assets/images/${this.props.img}`)} alt={this.props.title} style={{maxWidth: "11rem"}}/>
+                    <img className="book-item" src={require(`../assets/images/${this.props.img}`)} alt={this.props.title} style={{width: "11rem"}}/>
                     <p className="book-item-title" style={{maxWidth: "11rem"}}>{this.props.title}</p>
                 </a>
             </div>
